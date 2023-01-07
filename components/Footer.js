71c4c2175ae30777f4e83c85a731b687be9de378
adamjunios <adamjunioselvaa@gmail.com>
2023-01-07 09:57:36 +0700

    landing page complete

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-200">
+        <div className="space-y-2 text-xs text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+        <div className="space-y-2 text-xs text-gray-800">
+          <h5 className="font-bold">COMUNITY</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+        </div>
+        <div className="space-y-2 text-xs text-gray-800">
+          <h5 className="font-bold">HOST</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Luxe</p>
+        </div>
+        <div className="space-y-2 text-xs text-gray-800">
+          <h5 className="font-bold">SUPPORT</h5>
+          <p>How Airbnb works</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }: any) {
   return (
@@ -48,7 +50,18 @@ export default function Home({ exploreData, cardsData }: any) {
               ))}
             </div>
           </section>
+          <section className="pt-5">
+            <LargeCard
+              img="https://links.papareact.com/4cj"
+              title="The Gratest Outdoors"
+              description="Wishlist created by Airbnb."
+              buttonText="Get Inspired"
+            />
+          </section>
         </main>
+
+        {/* footer */}
+        <Footer />
       </div>
     </>
   );
